90f0d96baa28be980157258fcd8dc80f1261314d
Drake0211 <aldisescobedo02@gmail.com>
2023-10-26 12:18:02 -0600

    nuevos cambios

--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"net/http"
 
+	"github.com/Drake0211/ORM_API_REST/db"
+	"github.com/Drake0211/ORM_API_REST/models"
 	"github.com/Drake0211/ORM_API_REST/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	db.DBconection()
+
+	db.DB.AutoMigrate(models.Tasks{})
+	db.DB.AutoMigrate(models.User{})
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", routes.HomeHandler)
 
